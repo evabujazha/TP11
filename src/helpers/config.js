8e4d9f68b156047c285db2a3087c9c82f5a88e08
evabujazha <evabujazha@gmail.com>
2023-02-21 22:45:21 -0300

    first commit

---
+++ src/helpers/config.js
@@ -0,0 +1,13 @@
+const EXP_TIME = 200000 ;
+const DATABASE_URL = '';
+const MONGO = {
+    connection: "",
+    options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+}
+const TWITTER_CONSUMER_KEY = ""
+const TWITTER_CONSUMER_SECRET = ""
+
+export default {EXP_TIME, DATABASE_URL, MONGO, TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET}
